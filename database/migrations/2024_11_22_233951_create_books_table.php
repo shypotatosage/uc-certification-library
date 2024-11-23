16899560b4422923f3f422c98599c1ef4fa1ffc9
Mimi <91705093+shypotatosage@users.noreply.github.com>
2024-11-23 07:41:13 +0700

    chore: create model -crms

---
+++ database/migrations/2024_11_22_233951_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('book_cover');
+            $table->string('name');
+            $table->string('description');
+            $table->string('primary_author');
+            $table->string('publisher');
+            $table->string('published_year');
+            $table->string('isbn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>
+            @yield('title')
+        </title>
+    </head>
+
+    <body style="background-color: #FAFAFA;">
+    </body>
+</html>
